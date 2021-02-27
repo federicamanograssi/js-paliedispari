e30ca249315b6a462e1a1caa49dfa3bdb06507cb
Federica <federica.manograssi@gmail.com>
2021-02-27 10:13:58 +0100

    Palindromo ok
    Funzione creata

---
+++ palindrome/js/master.js
@@ -0,0 +1,30 @@
+// Chiedere all'utente di inserire una parola
+var userWord = prompt('Inserisci una parola');
+console.log(userWord);
+
+// // Creare una funzione per capire se la parola inserita è palindroma
+
+function isPalindrome(x) {
+
+    for (var i = 0; i < x.length; i++){
+        var start = x.charAt(i);
+        var end = x.charAt(x.length-(i+1));
+        console.log(start, end);
+
+        if (start != end){
+            console.log('false')
+            return false;
+        }
+    }
+    console.log('true')
+    return true;
+}  ;
+
+
+// ------------------PROVIAMO LA FUNZIONE----------------
+isPalindrome(userWord);
+if (isPalindrome(userWord) == true){
+    alert('è palindromo')
+}else{
+    alert('non è palindromo')
+}
