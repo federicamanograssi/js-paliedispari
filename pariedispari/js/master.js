28ca93c08f0264c619b739c1a6cbaca1b9903268
Federica <federica.manograssi@gmail.com>
2021-02-28 20:42:23 +0100

    CONSEGNA

--- pariedispari/js/master.js
+++ pariedispari/js/master.js
@@ -1,6 +1,7 @@
 // far puntare pari o dispari
 var pari = "pari";
 var dispari = "dispari";
+
 do {
     var userBet = prompt('Scegli: pari o dispari?')
 } while (userBet.slice().toLowerCase() != pari && userBet.slice().toLowerCase() != dispari )
@@ -9,32 +10,51 @@ console.log(userBet, userBet.charAt().toUpperCase()+userBet.slice(1).toLowerCase
 
 // far inserire un numero da 1 a 5
 do {
-    var userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
-} while ((userNumber < 1) || (userNumber > 5));
+    var userNumber = prompt('Inserisci un numero da 1 a 5');
+} while ((userNumber < 1) || (userNumber > 5) || isNaN(userNumber) || userNumber.includes("."));
 console.log(userNumber);
+userNumber = parseInt(userNumber);
+console.log(userNumber);
+/* mettere il parseInt sul prompt non impediva all'utonto di mettere un numero con i decimali
+all'interno del mio intervallo. in questo modo può mettere solo caratteri numerici senza punto,virgola o altri segni*/
 
 
-// generare un numero casuale da 1 a 5
-var randomNumber = Math.floor(Math.random()*(5 - 1))+ 1;
-alert('io tiro ' + randomNumber)
-console.log(randomNumber);
-
-// sommare i due numeri
-var sum = randomNumber + userNumber;
-console.log(sum);
-
-// verificare se la somma è pari o dispari
-if (sum % 2 == 0){
-    sum = "pari";
-    console.log('pari')
-} else {
-    sum = "dispari";
-    console.log('dispari')
+// generare un numero casuale da 1 a 5 CON FUNZIONE
+function randomNumber(min,max) {
+    var result = Math.floor(Math.random()*(max - min))+ min;
+    return result;
 }
 
+var myNumber = randomNumber(1,5);
+alert('io tiro ' + myNumber)
+console.log(myNumber);
+
+// sommare i due numeri
+var sum = myNumber + userNumber;
+console.log(sum);
+
+// verificare se la somma è pari o dispari CON FUNZIONE
+function isEven(x){
+    if (x % 2 != 0){
+        return false;
+    }
+    return true
+};
+
+isEven(sum);
+console.log(isEven(sum))
+
+if(isEven(sum) == true){
+    isEven(sum) == pari;
+};
+if(isEven(sum) == false){
+    isEven(sum) == dispari;
+};
+
+
 // verificare se ha vinto
-if (sum == userBet) {
-    alert('La somma è ' + sum + ' . You won')
+if (isEven(sum) == userBet) {
+    alert('La somma è ' + sum + '. You won')
 } else {
-    alert('La somma è ' + sum + ' . You lose')
+    alert('La somma è ' + sum + '. You lose')
 }
